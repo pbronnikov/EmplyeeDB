efef5e02f6c0b4b3987b4d30e1b9eea9146fc094
Pavel Bronnikov <pbronnikov@bostontechnologies.com>
2013-04-02 16:34:29 +0600

    Added module DAL
    Also added dump implementation of DataLayer class

---
+++ DAL/src/DataLayer.scala
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pbronnikov
+ * Date: 02.04.13
+ * Time: 16:30
+ * To change this template use File | Settings | File Templates.
+ */
+class DataLayer extends DataLayerInterface{
+
+  def edit(employee: Employee): Int = 0
+
+  def getAll: List[Employee] = null
+
+  def getByID(id: Int): Employee = null
+
+  def search(criteria: String): List[Employee] = null
+
+  def editDayOff(dayOff: DayOff, employeeID: Int) {}
+
+  def deleteDayOff(dayOff: DayOff, employeeID: Int) {}
+}
