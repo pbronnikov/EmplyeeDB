37dc9ee6ac511e9e9da77e64fc6ba6d85681dbaa
Pavel Bronnikov <pbronnikov@bostontechnologies.com>
2013-04-02 17:57:13 +0600

    Added Database shema

---
+++ Database/CreateDB.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\MySql\MySQL Server 5.6\bin\mysql.exe" -h localhost -u root -ptoor < Schema.sql
---
+++ Database/Schema.sql
@@ -0,0 +1,36 @@
+drop database if exists EmployerDB_Test;
+create database EmployerDB_Test;
+use EmployerDB_Test;
+
+CREATE TABLE Employee (
+         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         firstName VARCHAR(100),
+		 middleName VARCHAR(100),
+		 lastName VARCHAR(100),
+		 skype VARCHAR(100),
+		 tel VARCHAR(100),
+		 email VARCHAR(100)
+       );
+
+CREATE TABLE DayOff(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	employee_id INT,
+	`from` DATETIME,
+	`to` DATETIME,
+	Description VARCHAR(255),
+	DayOffType SMALLINT,
+
+	FOREIGN KEY (employee_id) REFERENCES Employee(id)
+);
+
+CREATE TABLE ProjectHistory(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	employee_id INT,
+	`from` DATETIME,
+	`to` DATETIME,
+	projectName VARCHAR(255),
+	role SMALLINT,
+
+	FOREIGN KEY (employee_id) REFERENCES Employee(id)
+
+);
