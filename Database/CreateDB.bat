5c105b0a0414704234ef5d1d1862f67ee01f1f5c
Pavel Bronnikov <pbronnikov@bostontechnologies.com>
2013-04-03 11:52:30 +0600

    Added tests data to DB

--- Database/CreateDB.bat
+++ Database/CreateDB.bat
@@ -1,1 +1,2 @@
 "C:\Program Files\MySql\MySQL Server 5.6\bin\mysql.exe" -h localhost -u root -ptoor < Schema.sql
+"C:\Program Files\MySql\MySQL Server 5.6\bin\mysql.exe" -h localhost -u root -ptoor < TestData.sql
