f67e34c5e4600fdef2362cc29b904ddbaa012225
Pavel Bronnikov <pbronnikov@bostontechnologies.com>
2013-04-09 10:13:38 +0600

    Changed DATATIME to TIMESTAMP
    Solving compatibility problems with Java sql types.

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,36 +1,36 @@
-drop database if exists EmployerDB_Test;
-create database EmployerDB_Test;
-use EmployerDB_Test;
+DROP DATABASE IF EXISTS EmployerDB_Test;
+CREATE DATABASE EmployerDB_Test;
+USE EmployerDB_Test;
 
 CREATE TABLE Employee (
-         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-         firstName VARCHAR(100),
-		 middleName VARCHAR(100),
-		 lastName VARCHAR(100),
-		 skype VARCHAR(100),
-		 tel VARCHAR(100),
-		 email VARCHAR(100)
-       );
-
-CREATE TABLE DayOff(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	employee_id INT,
-	`from` Timestamp,
-	`to` Timestamp,
-	Description VARCHAR(255),
-	DayOffType SMALLINT,
-
-	FOREIGN KEY (employee_id) REFERENCES Employee(id)
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstName  VARCHAR(100),
+  middleName VARCHAR(100),
+  lastName   VARCHAR(100),
+  skype      VARCHAR(100),
+  tel        VARCHAR(100),
+  email      VARCHAR(100)
 );
 
-CREATE TABLE ProjectHistory(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	employee_id INT,
-	`from` DATETIME,
-	`to` DATETIME,
-	projectName VARCHAR(255),
-	role SMALLINT,
+CREATE TABLE DayOff (
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_id INT,
+  `from`      TIMESTAMP,
+  `to`        TIMESTAMP,
+  Description VARCHAR(255),
+  DayOffType  SMALLINT,
 
-	FOREIGN KEY (employee_id) REFERENCES Employee(id)
+  FOREIGN KEY (employee_id) REFERENCES Employee (id)
+);
+
+CREATE TABLE ProjectHistory (
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_id INT,
+  `from`      TIMESTAMP,
+  `to`        TIMESTAMP,
+  projectName VARCHAR(255),
+  role        SMALLINT,
+
+  FOREIGN KEY (employee_id) REFERENCES Employee (id)
 
 );
