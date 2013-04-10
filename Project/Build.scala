689ee94e2d3acefb5b5e741efcbaa794849c939b
Ilya Solo <ilya.i.solo@gmail.com>
2013-04-10 11:50:18 +0600

    Added necessary package dependencies
    Added config library for App configuration class

--- Project/Build.scala
+++ Project/Build.scala
@@ -6,16 +6,19 @@ object ProjBuild extends Build {
 
   val customResolvers = Seq(twitterRepo)
 
-       val deps = Seq("net.databinder" % "unfiltered-filter_2.9.1" % "0.5.3",
-                      "net.databinder" % "unfiltered-jetty_2.9.1" % "0.5.3",
-		      "org.scalaz" %% "scalaz-core" % "6.0.4",
-                      "com.twitter" % "querulous" % "2.1.5",
-		      "net.liftweb" %% "lift-json" % "2.4")
+  val deps = Seq(
+    "net.databinder" % "unfiltered-filter_2.9.1" % "0.5.3",
+    "net.databinder" % "unfiltered-jetty_2.9.1" % "0.5.3",
+    "org.scalaz" %% "scalaz-core" % "6.0.4",
+    "com.twitter" % "querulous" % "2.1.5",
+    "net.liftweb" %% "lift-json" % "2.4",
+    "com.typesafe" % "config" % "1.0.0"
+  )
 
-       val root = Project(
-           "EmployeeDB",
-           file("."),
-	   settings = Defaults.defaultSettings ++ Seq(scalaVersion := "2.9.1", libraryDependencies ++= deps, resolvers ++= customResolvers),
-           configurations = Configurations.default
-        )
+  val root = Project(
+    "EmployeeDB",
+    file("."),
+    settings = Defaults.defaultSettings ++ Seq(scalaVersion := "2.9.1", libraryDependencies ++= deps, resolvers ++= customResolvers),
+    configurations = Configurations.default
+  )
 }
