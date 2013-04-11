4dcdec869c03070ff9b8483aaa3f6b256e09ef98
Ilya Solo <ilya.i.solo@gmail.com>
2013-04-11 11:02:32 +0600

    Edited bash scripts
    Now bash scripts parsing config file and get host and port

--- src/Testing/edit.sh
+++ src/Testing/edit.sh
@@ -1,3 +1,7 @@
 line=$(sed -n 2p test)
-curl -d $line localhost:9091/edit
+
+h=$(cat ../Main/resources/application.conf  | grep host | sed -n 2p | sed 's/[^0-9.]//g')
+p=$(cat ../Main/resources/application.conf  | grep port | sed -n 2p | sed 's/[^0-9.]//g')
+
+curl -d $line $h:$p/edit
 echo
