1147d2894d4b31a9ec15df0be1a9e5525af0631d
Vitaly Goryaev <lacrom@mail.ru>
2013-04-11 11:31:04 +0600

    Added validation handling

--- src/Main/scala/ServiceLayer/RequestHandler.scala
+++ src/Main/scala/ServiceLayer/RequestHandler.scala
@@ -5,100 +5,110 @@
  * Time: 15:13
  * To change this template use File | Settings | File Templates.
  */
-
 import net.liftweb.json._
 import net.liftweb.json.Serialization._
+import scalaz.{Failure, Success}
 
 class RequestHandler(logicInstance: EmployeeLogic) {
   implicit val formats = Serialization.formats(NoTypeHints)
 
   def create(requestBody: String): String = {
     val e: Employee = read[Employee](requestBody)
-    if (e != None) {
-      logicInstance.create(e)
-      "Employee created"
-    } else
+    if (e != None)
+      logicInstance.create(e) match {
+        case Success(_) => "Employee created"
+        case Failure(f) => f.getMessage
+      }
+    else
       "Error parsing Employee"
   }
 
   def edit(requestBody: String): String = {
     val e: Employee = read[Employee](requestBody)
-    if (e != None) {
-      logicInstance.edit(e)
-      "Employee edited"
-    } else
+    if (e != None)
+      logicInstance.edit(e) match {
+        case Success(_) => "Employee edit"
+        case Failure(f) => f.getMessage
+      }
+    else
       "Error parsing Employee"
   }
 
   def archive(requestBody: String): String = {
-    if (requestBody.contains(Int)) {
+    if (requestBody.forall(_.isDigit)) {
       val id: Int = Integer.parseInt(requestBody)
-      logicInstance.archive(id)
-    }
-    "Error: method not implemented"
+      logicInstance.archive(id) match {
+        case Success(_) => "Employee " + id + " archived"
+        case Failure(f) => f.getMessage
+      }
+    } else
+      "Body is not a number"
   }
 
   def getAll: String = {
-    val employeeList = logicInstance.getAll
-    if (employeeList != None) {
-      write(employeeList)
-    } else
-      "List is empty"
+    logicInstance.getAll match {
+      case Success(employeeList) => write(employeeList)
+      case Failure(f) => f.getMessage
+    }
   }
 
   def getByID(requestBody: String): String = {
     if (requestBody.forall(_.isDigit)) {
       val id: Int = Integer.parseInt(requestBody)
-      val employee = logicInstance.getByID(id)
-      if (employee != None)
-        write(employee)
-      else
-        "No such employee"
+      logicInstance.getByID(id) match {
+        case Success(employee) => write(employee)
+        case Failure(f) => f.getMessage
+      }
     } else
       "Body is not a number"
   }
 
   def search(criteria: String): String = {
-    val employeeList = logicInstance.search(criteria)
-    if (employeeList != None) {
-      write(employeeList)
-    } else
-      "List is empty"
+    logicInstance.search(criteria) match {
+      case Success(employeeList) => write(employeeList)
+      case Failure(f) => f.getMessage
+    }
   }
 
   def addDayOff(requestBody: String, id: String): String = {
     val d: DayOff = read[DayOff](requestBody)
-    if (id.forall(_.isDigit))
+    if (id.forall(_.isDigit)) {
       if (d != None) {
-        logicInstance.addDayOff(d, Integer.parseInt(id))
-        "DayOff added"
+        logicInstance.addDayOff(d, Integer.parseInt(id)) match {
+          case Success(_) => "DayOff added"
+          case Failure(f) => f.getMessage
+        }
       } else
-        "Incorrect dayOff message"
-    else
+        "Can not parse DayOff"
+    } else
       "Incorrect id"
   }
 
   def editDayOff(requestBody: String, id: String): String = {
     val d: DayOff = read[DayOff](requestBody)
-    if (id.forall(_.isDigit))
+    if (id.forall(_.isDigit)) {
       if (d != None) {
-        logicInstance.editDayOff(d, Integer.parseInt(id))
-        "DayOff edited"
+        logicInstance.editDayOff(d, Integer.parseInt(id)) match {
+          case Success(_) => "DayOff edited"
+          case Failure(f) => f.getMessage
+        }
       } else
-        "Incorrect dayOff message"
-    else
-      "Incorrect id"
+        "Can not parse DayOff"
+    } else
+        "Incorrect id"
   }
 
   def deleteDayOff(requestBody: String, id: String): String = {
     val d: DayOff = read[DayOff](requestBody)
-    if (id.forall(_.isDigit))
+    if (id.forall(_.isDigit)) {
       if (d != None) {
-        logicInstance.deleteDayOff(d /*, Integer.parseInt(id)*/)
-        "DayOff deleted"
+        logicInstance.deleteDayOff(d) match {
+          case Success(_) => "DayOff deleted"
+          case Failure(f) => f.getMessage
+        }
       } else
-        "Incorrect dayOff message"
-    else
+        "Can not parse DayOff"
+    } else
       "Incorrect id"
   }
 }
