79bcef9ab4419a9d0a94c65c4fdc3e4e86ab6ef4
Ilya Solo <ilya.i.solo@gmail.com>
2013-04-16 10:58:47 +0600

    Some fixes in bash testing scripts
    Fixed incorrect port parsing

--- src/Testing/create.sh
+++ src/Testing/create.sh
@@ -1,6 +1,6 @@
 line=$(sed -n 2p test)
 h=$(cat ../Main/resources/application.conf  | grep host | sed -n 2p | sed 's/[^0-9.]//g')
-p=$(cat ../Main/resources/application.conf  | grep port | sed -n 2p | sed 's/[^0-9.]//g')
+p=$(cat ../Main/resources/application.conf  | grep port | sed -n 1p | sed 's/[^0-9.]//g')
 
 curl -d $line $h:$p/create
 echo
--- src/Testing/search.sh
+++ src/Testing/search.sh
@@ -1,5 +1,5 @@
 h=$(cat ../Main/resources/application.conf  | grep host | sed -n 2p | sed 's/[^0-9.]//g')
-p=$(cat ../Main/resources/application.conf  | grep port | sed -n 2p | sed 's/[^0-9.]//g')
+p=$(cat ../Main/resources/application.conf  | grep port | sed -n 1p | sed 's/[^0-9.]//g')
 
 echo -n 'Input employee field = '
 read i
